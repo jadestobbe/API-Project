5ba3d530e0bdeaf8ca7b9bbbe27ee6d93006c135
Jade Stobbe <jadestobbe@gmail.com>
2016-04-15 14:20:26 -0500

    changed directory to dist instead of src

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var http = require('http');
 var serveStatic = require('serve-static');
 
 var app = connect();
-var serve = serveStatic('./src');
-app.use(serveStatic('./src'));
+app.use(serveStatic('./dist'));
 
 console.log('Starting webserver on http://localhost:8080/');
 http.createServer(app).listen(8080);
